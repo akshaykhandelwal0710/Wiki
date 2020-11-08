40b0192910a9ba5867fdc710cb8ae1d1fee3285d
akshaykhandelwal0710 <39028584+akshaykhandelwal0710@users.noreply.github.com>
2020-11-08 15:10:26 +0530

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name = "index"), 
     path("new_entry", views.new, name = "new"), 
     path("random_entry", views.rand, name = "random"), 
-    path("wiki/<str:title>", views.index, name = "title")
+    path("wiki/<str:title>", views.index, name = "title"), 
+    path("edit/<str:entry>", views.edit, name = "edit")
 ]
